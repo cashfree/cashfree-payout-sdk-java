0a79a1c8e5e3b8c247819a9adf06e827cebe6278
bitbucket-pipelines <commits-noreply@bitbucket.org>
2024-03-11 12:02:19 +0000

    Generating Java SDK.

--- src/main/java/com/cashfree/model/FetchBatchTransferResponse.java
+++ src/main/java/com/cashfree/model/FetchBatchTransferResponse.java
@@ -55,7 +55,7 @@ import com.cashfree.JSON;
  * Batch Transfer Status V2 Response
  */
 @Schema(description = "Batch Transfer Status V2 Response")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-03-08T12:01:17.874167Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-03-11T12:02:16.890239Z[Etc/UTC]")
 public class FetchBatchTransferResponse {
   public static final String SERIALIZED_NAME_BATCH_TRANSFER_ID = "batch_transfer_id";
   @SerializedName(SERIALIZED_NAME_BATCH_TRANSFER_ID)
